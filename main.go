ec89f282be938bb218d827d7e83b2e7545d08e29
Fotis Sofoulis <fotis.sofoulis@gmail.com>
2025-10-02 15:35:08 +0200

    Added handlers, struct and methods. also a logo

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
-package main 
+package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
 const (
@@ -10,16 +12,50 @@ const (
 	port = "8080"
 )
 
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricIncrease(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) displayMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	fmt.Fprintf(w, "Hits: %d", cfg.fileServerHits.Load())
+} 
+
+func (cfg *apiConfig) resetMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	cfg.fileServerHits.Swap(0)
+}
+
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	cfg := &apiConfig{}
+	appHandler :=  http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", cfg.middlewareMetricIncrease(appHandler))
 
 	server := &http.Server{
 		Addr: ":" + port,
 		Handler: mux,
 	}
 	
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	mux.HandleFunc("/metrics", cfg.displayMetrics)
+	mux.HandleFunc("/reset", cfg.resetMetrics)
+
+
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
+
 
 }
