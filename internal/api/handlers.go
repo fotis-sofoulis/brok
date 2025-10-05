3ac4606bd4c4aefb976a2cb197177b68d680e626
Fotis Sofoulis <fotis.sofoulis@gmail.com>
2025-10-05 22:34:29 +0200

    code refactor part 2

--- internal/api/handlers.go
+++ internal/api/handlers.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"sync/atomic"
@@ -27,4 +28,25 @@ func (cfg *ApiConfig) DisplayMetrics(w http.ResponseWriter, r *http.Request) {
 func (cfg *ApiConfig) ResetMetrics(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	cfg.FileServerHits.Swap(0)
+}
+
+func (cfg *ApiConfig) ValidateChirp(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+
+	type parameters struct {
+		Body string `json:"body"`
+	}
+
+	var params parameters
+	if err := json.NewDecoder(r.Body).Decode(&params); err != nil {
+		RespondWithError(w, http.StatusBadRequest, "Something went wrong")
+		return
+	}
+
+	if len(params.Body) > 140 {
+		RespondWithError(w, http.StatusBadRequest, "Chirp is too long")
+		return
+	}
+
+	RespondWithJSON(w, http.StatusOK, CleanedResp{Cleaned: ""})
 }
--- main.go
+++ main.go
@@ -31,60 +31,7 @@ func main() {
 		w.Write([]byte("OK"))
 	})
 
-	mux.HandleFunc("POST /api/validate_chirp", func(w http.ResponseWriter, r *http.Request) {
-
-		defer r.Body.Close()
-
-		type parameters struct {
-			Body string `json:"body"`
-		}
-
-		type errorResp struct {
-			Error string `json:"error"`
-		}
-
-		type validResp struct {
-			Valid bool `json:"valid"`
-		}
-		
-		w.Header().Set("Content-Type", "application/json")
-
-		params := parameters{}
-		if err := json.NewDecoder(r.Body).Decode(&params); err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			msg, mErr := json.Marshal(errorResp{Error: "Something went wrong"})
-			if mErr != nil {
-				w.Header().Del("Content-Type")
-				io.WriteString(w, "Something went really wrong")
-				return
-			}
-			w.Write(msg)
-			return
-		}
-
-		if len(params.Body) > 140 {
-			w.WriteHeader(http.StatusBadRequest)
-			msg, mErr := json.Marshal(errorResp{Error: "Chirp is too long"})
-			if mErr != nil {
-				w.Header().Del("Content-Type")
-				io.WriteString(w, "Something went really wrong")
-				return
-			}
-			w.Write(msg)
-			return
-		}
-
-		resp, err := json.Marshal(validResp{Valid: true})
-		if err != nil {
-			w.Header().Del("Content-Type")
-			io.WriteString(w, "Something went really wrong")
-			return
-		}
-		
-		w.WriteHeader(http.StatusOK)
-		w.Write(resp)
-	})
-
+	mux.HandleFunc("POST /api/validate_chirp", cfg.ValidateChirp)
 	mux.HandleFunc("GET /admin/metrics", cfg.DisplayMetrics)
 	mux.HandleFunc("POST /admin/reset", cfg.ResetMetrics)
 
