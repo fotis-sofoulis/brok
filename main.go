ee77bd13392485d460b290cbfc7f84ca5e6dd3c6
Fotis Sofoulis <fotis.sofoulis@gmail.com>
2025-10-05 19:47:57 +0200

    added new endpoint

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"sync/atomic"
@@ -56,6 +58,60 @@ func main() {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
+	})
+
+	mux.HandleFunc("POST /api/validate_chirp", func(w http.ResponseWriter, r *http.Request) {
+
+		defer r.Body.Close()
+
+		type parameters struct {
+			Body string `json:"body"`
+		}
+
+		type errorResp struct {
+			Error string `json:"error"`
+		}
+
+		type validResp struct {
+			Valid bool `json:"valid"`
+		}
+		
+		w.Header().Set("Content-Type", "application/json")
+
+		params := parameters{}
+		if err := json.NewDecoder(r.Body).Decode(&params); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			msg, mErr := json.Marshal(errorResp{Error: "Something went wrong"})
+			if mErr != nil {
+				w.Header().Del("Content-Type")
+				io.WriteString(w, "Something went really wrong")
+				return
+			}
+			w.Write(msg)
+			return
+		}
+
+		if len(params.Body) > 140 {
+			w.WriteHeader(http.StatusBadRequest)
+			msg, mErr := json.Marshal(errorResp{Error: "Chirp is too long"})
+			if mErr != nil {
+				w.Header().Del("Content-Type")
+				io.WriteString(w, "Something went really wrong")
+				return
+			}
+			w.Write(msg)
+			return
+		}
+
+		resp, err := json.Marshal(validResp{Valid: true})
+		if err != nil {
+			w.Header().Del("Content-Type")
+			io.WriteString(w, "Something went really wrong")
+			return
+		}
+		
+		w.WriteHeader(http.StatusOK)
+		w.Write(resp)
 	})
 
 	mux.HandleFunc("GET /admin/metrics", cfg.displayMetrics)
